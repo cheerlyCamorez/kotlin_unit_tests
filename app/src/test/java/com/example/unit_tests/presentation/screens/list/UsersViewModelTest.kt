1a44f8d055d0d5856707f17f62add296ddfeb0bb
Sergey Batishchev <Sergei.Batishchevv@yandex.ru>
2024-04-16 18:20:13 +0300

    final version

--- app/src/test/java/com/example/unit_tests/presentation/screens/list/UsersViewModelTest.kt
+++ app/src/test/java/com/example/unit_tests/presentation/screens/list/UsersViewModelTest.kt
@@ -1,10 +1,11 @@
 package com.example.unit_tests.presentation.screens.list
 
+
 import com.example.unit_tests.data.database.entity.user.User
 import com.example.unit_tests.domain.useCases.getUserListUseCase
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.AfterEach
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.mock
+
 
 class UsersViewModelTest {
     private val getUserListUseCase = mock<getUserListUseCase>()
@@ -21,18 +23,53 @@ class UsersViewModelTest {
 
     }
     @Test
-    fun `getUsersList should update Users and FilteredUsers`() {
-        Dispatchers.setMain(Dispatchers.Default)
-        val viewModel = UsersViewModel(getUserListUseCase)
+    fun `1 getUsersList should update Users and FilteredUsers`() {
+
+
         val userList = listOf(User(login = "testUser1", pwd = "testUser1", userName = "testUser1"))
 
-        Mockito.`when`(getUserListUseCase.invoke()).thenReturn(flowOf(userList))
-
-        viewModel.changeFilterText("")
-
-        assertEquals(userList, viewModel.FilteredUsers.value)
+        Mockito.`when`(getUserListUseCase.invoke()).thenReturn(MutableStateFlow(userList))
+        Dispatchers.setMain(TestCoroutineDispatcher())
+        val viewModel = UsersViewModel(getUserListUseCase)
+        viewModel.changeFilterText("testUser1")
         Dispatchers.resetMain()
 
-    }
+        assertEquals(userList, viewModel.FilteredUsers.value)
 
+    }
+    @Test
+    fun `2 getUsersList should return empty filtered list  `() {
+
+
+        val userList = listOf(User(login = "testUser1", pwd = "testUser1", userName = "testUser1"))
+        val emptyUserList : List<User> = emptyList()
+        Mockito.`when`(getUserListUseCase.invoke()).thenReturn(MutableStateFlow(userList))
+        Dispatchers.setMain(TestCoroutineDispatcher())
+        val viewModel = UsersViewModel(getUserListUseCase)
+        viewModel.changeFilterText("testUser2")
+        Dispatchers.resetMain()
+        //проверка на вывод пустого списка, так как по фильтру такого нет
+        assertEquals(emptyUserList, viewModel.FilteredUsers.value)
+        //вывод фильтра
+        assertEquals("testUser2" , viewModel.filterText.value)
+    }
+    @Test
+    fun `3 getUsersList should return 1 element of filtered list`() {
+
+
+        val userList = listOf(
+            User(login = "testUser1", pwd = "testUser1", userName = "testUser1"),
+            User(login = "testUser2", pwd = "testUser2", userName = "testUser2")
+        )
+
+        Mockito.`when`(getUserListUseCase.invoke()).thenReturn(MutableStateFlow(userList))
+        Dispatchers.setMain(TestCoroutineDispatcher())
+        val viewModel = UsersViewModel(getUserListUseCase)
+        viewModel.changeFilterText("testUser2")
+        Dispatchers.resetMain()
+        //проверка на вывод пустого списка, так как по фильтру такого нет
+        assertEquals(listOf(userList[1]), viewModel.FilteredUsers.value)
+        //вывод фильтра
+        assertEquals("testUser2" , viewModel.filterText.value)
+    }
 }
